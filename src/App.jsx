12997c20aee0dbf545ba6324bff284a25a5b9b29
Radha <radhasharma2786@gmail.com>
2024-03-12 23:24:19 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import  React, { useEffect, useState } from "react"
+import { Editor } from '@tinymce/tinymce-react';
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth"
 import './App.css'
@@ -25,6 +26,15 @@ function App() {
 
 
 return !loading ? (
+  <>
+  <Editor
+      apiKey='lsdwj22tz40d1pedrd5oaafnbth4381wofxm3ld2qh2pqqik'
+      init={{
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+      }}
+      initialValue="Welcome to TinyMCE!"
+    />
   <div className="min-h-screen flex flex-wrap content-between bg-fuchsia-100">
     <div className="w-full block">
       <Header/>
@@ -35,6 +45,7 @@ return !loading ? (
 
     </div>
   </div>
+  </>
 ) : <h1>Loading...</h1>
 }
 
